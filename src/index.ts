62d88b14c8dba4bb3d6cdbebe08e28c3c4920f0b
Alexeykoh <alexeykoh@gmail.com>
2024-04-16 21:40:46 +0300

    refactor: prepare to merge

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { IOElement } from './IO/io-element';
-
-const test = new IOElement({ tag: 'p', text: 'hello world' });
-test.render();
-
-document.body.appendChild(test.getElement());
-
-console.log(test);
