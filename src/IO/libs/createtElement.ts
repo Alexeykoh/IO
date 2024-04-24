a81979de755741a94e7e30868ff773f5a861a7c2
Alexeykoh <Alexeykoh@icloud.com>
2024-04-24 11:24:03 +0300

    feat: create conditional compare

--- src/IO/libs/createtElement.ts
+++ src/IO/libs/createtElement.ts
@@ -51,11 +51,50 @@ function writeAttributeList(element: HTMLElement, attributeList: _attributeList)
 
 export function CreateElement(params: IONode) {
     const element = document.createElement(params.tag);
+    element.id = params.id;
+    writeText(element, params.text);
+    writeClassList(element, params.classList);
+    writeAttributeList(element, params.attributeList);
+    writeEventList(element, params.eventList);
+    return element;
+}
+
+export function UpdateElement(params: IONode, element: HTMLElement) {
     // element.setAttribute('io-id', params.id);
     element.id = params.id;
     writeText(element, params.text);
     writeClassList(element, params.classList);
     writeAttributeList(element, params.attributeList);
     writeEventList(element, params.eventList);
+    return element;
+}
+
+export function UpdateElementConditional(current: IONode, snapshot: IONode) {
+    // creating new element
+    const element = document.createElement(current.tag);
+    element.id = current.id;
+    // conditional comparing
+    const text = current.text === snapshot.text;
+    const classList =
+        JSON.stringify(current.classList) || undefined === JSON.stringify(snapshot.classList) || undefined;
+    const eventList =
+        JSON.stringify(current.eventList) || undefined === JSON.stringify(snapshot.eventList) || undefined;
+    const attributeList =
+        JSON.stringify(current.attributeList) || undefined === JSON.stringify(snapshot.attributeList) || undefined;
+
+    // conditional rewriting
+    if (!text) {
+        writeText(element, current.text);
+    }
+    if (!classList) {
+        writeClassList(element, current.classList);
+    }
+    if (!eventList) {
+        writeEventList(element, current.eventList);
+    }
+    if (!attributeList) {
+        writeAttributeList(element, current.attributeList);
+    }
+
     return element;
 }
