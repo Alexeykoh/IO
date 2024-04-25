dd326fd64130faab411abbf5e5c637e12d116e39
Alexeykoh <alexeykoh@gmail.com>
2024-04-25 20:35:35 +0300

    refactor: replace & rename

--- src/IO/libs/router/routes/dynamic-route.io.ts
+++
@@ -1,8 +0,0 @@
-import { iRouteParams, routeIO } from '../types/types';
-import { Route } from './route.io';
-
-export class DynamicRoute extends Route {
-    constructor(route: routeIO, path: `/${string}`, params: iRouteParams, name?: string | undefined) {
-        super(route, path, params, name);
-    }
-}
