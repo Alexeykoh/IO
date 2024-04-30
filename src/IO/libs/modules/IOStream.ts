fcc119eaab6ac9062ad88911979fa78817cfae18
Alexeykoh <Alexeykoh@icloud.com>
2024-04-30 15:54:39 +0300

    refactor: remove, rename & replace

--- src/IO/libs/modules/IOStream.ts
+++
@@ -1,8 +0,0 @@
-import { Stream } from '../../utils/stream';
-
-export interface iStreamMessage {
-    id: string;
-    data: unknown;
-}
-
-export const $IOStream = new Stream<iStreamMessage>();
--- src/IO/libs/router/authentication/auth-page/_ui/_label.ts
+++
@@ -1,8 +0,0 @@
-import { IO } from '../../../../modules/IO';
-import { tag } from '../../../../modules/types.io';
-
-export function Label(title: string, children: (() => IO)[]) {
-    const io = new IO(tag.LABEL);
-    io.components = [() => new IO(tag.P, { text: title }), ...children];
-    return io;
-}
