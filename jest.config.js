02e00621a8703d1f127fe7bfb7d90e597daca5cb
Alexeykoh <Alexeykoh@icloud.com>
2024-05-03 16:12:13 +0300

    feat: add jest dependencies

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Add your Jest configuration here
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+};
