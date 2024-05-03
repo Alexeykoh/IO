ed99c4d576fe1a9afa0be6321986bf2c40de875c
Alexeykoh <Alexeykoh@icloud.com>
2024-05-03 15:45:59 +0300

    refactor: replace render on update to mutation

--- src/IO/libs/modules/re-rendering/re-rendering.module.ts
+++ src/IO/libs/modules/re-rendering/re-rendering.module.ts
@@ -1,22 +1,27 @@
 import { IO } from '../../../IO';
 import { getRef } from '../../helpers/get-ref';
+import { Hydration } from '../hydration/hydration.module';
 
 export class ReRendering {
+    // root
     private _node: IO;
 
+    // module
+    private _hydration: Hydration;
+
     constructor(node: IO) {
+        // root
         this._node = node;
+
+        // module
+        this._hydration = new Hydration();
     }
 
-    public update() {
+    public update(): void {
         // Get the component in the DOM using its element ID
         const componentInDOM = getRef(this._node.elementID);
 
-        // Render the new component
-        const newComponent = this._node.render();
-        newComponent.focus();
-
-        // Replace the existing component in the DOM with the new one
-        componentInDOM?.replaceWith(newComponent);
+        // recursive mutate
+        this._hydration.mutate(this._node, componentInDOM);
     }
 }
