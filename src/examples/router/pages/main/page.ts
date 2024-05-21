78961ad93a063bb82a2bbd6a582198d362670435
Alexeykoh <Alexeykoh@icloud.com>
2024-05-21 15:58:27 +0300

    IO-86byvpbca-svg/feat: add svg compatibility

--- src/examples/router/pages/main/page.ts
+++ src/examples/router/pages/main/page.ts
@@ -2,24 +2,6 @@ import { IO } from '../../../../IO/IO';
 import { tag } from '../../../../IO/libs/types/types.io';
 
 export function MainPage() {
-    const io = new IO(tag.SECTION, { text: 'main' }, [StreamComponent, NotifyComponent]);
-    return io;
-}
-
-function StreamComponent() {
-    const io = new IO(tag.DIV);
-    io.text = 'streamer';
-    io.stream().subscribe('123', () => {});
-    return io;
-}
-
-function NotifyComponent() {
-    const io = new IO(tag.BUTTON);
-    io.text = 'notifier';
-    io.events = {
-        click: () => {
-            io.stream().notify('123', 'hello streamer');
-        },
-    };
+    const io = new IO(tag.SECTION);
     return io;
 }
--- src/examples/router/shared/layout.ts
+++ src/examples/router/shared/layout.ts
@@ -1,10 +1,9 @@
 import { IO } from '../../../IO/IO';
 import { tag } from '../../../IO/libs/types/types.io';
-import { Footer } from './footer';
 import { Header } from './header';
 
 export function Layout(children: (() => IO)[]) {
     const io = new IO(tag.MAIN);
-    io.components = [Header, ...children, Footer];
+    io.components = [Header, ...children];
     return io;
 }
